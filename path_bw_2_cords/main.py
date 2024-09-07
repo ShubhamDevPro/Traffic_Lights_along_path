5f045ed3ab79a5382aaed0234fd6305ae1daf3f9
Shubham Dev <devshubham22@gmail.com>
2024-09-07 18:43:50 +0530

    new

--- path_bw_2_cords/main.py
+++ path_bw_2_cords/main.py
@@ -4,8 +4,9 @@ from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 
 app = Flask(__name__)
-OSRM_BASE_URL = 'http://127.0.0.1:5001'
+OSRM_BASE_URL = 'https://osrm-service-196751621509.asia-south1.run.app'
 geolocator = Nominatim(user_agent="my_route_finder")
+
 
 def get_coordinates(address):
     """Use Nominatim geocoding service to get coordinates for an address."""
@@ -18,6 +19,7 @@ def get_coordinates(address):
     except (GeocoderTimedOut, GeocoderServiceError):
         return None
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -25,15 +27,15 @@ def index():
         end_address = request.form['end_address']
         start_coords = get_coordinates(start_address)
         end_coords = get_coordinates(end_address)
-        
+
         if start_coords and end_coords:
             osrm_url = f'{OSRM_BASE_URL}/route/v1/driving/{start_coords[1]},{start_coords[0]};{end_coords[1]},{end_coords[0]}?steps=true&geometries=geojson'
-            
+
             response = requests.get(osrm_url)
             if response.status_code == 200:
                 route_data = response.json()
-                return render_template('index.html', route_data=route_data, 
-                                       start_address=start_address, 
+                return render_template('index.html', route_data=route_data,
+                                       start_address=start_address,
                                        end_address=end_address)
             else:
                 return "Error getting route from OSRM"
@@ -41,17 +43,18 @@ def index():
             return "One or both addresses could not be geocoded"
     return render_template('index.html')
 
+
 @app.route('/get_route', methods=['POST'])
 def get_route():
     start_address = request.json['start_address']
     end_address = request.json['end_address']
-    
+
     start_coords = get_coordinates(start_address)
     end_coords = get_coordinates(end_address)
-    
+
     if start_coords and end_coords:
         osrm_url = f'{OSRM_BASE_URL}/route/v1/driving/{start_coords[1]},{start_coords[0]};{end_coords[1]},{end_coords[0]}?steps=true&geometries=geojson'
-        
+
         response = requests.get(osrm_url)
         if response.status_code == 200:
             route_data = response.json()
@@ -61,5 +64,6 @@ def get_route():
     else:
         return jsonify({"error": "One or both addresses could not be geocoded"}), 400
 
+
 if __name__ == '__main__':
-    app.run(debug=True,port = 5009)
+    app.run(debug=True, port=5009)
